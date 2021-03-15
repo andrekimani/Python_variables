d5103311d4d98086ea60fbbc1eda6f3f4145f223
andrekimani <andrekimani@gmail.com>
2021-03-15 23:27:02 +0300

    First commit

---
+++ Variables.py
@@ -0,0 +1,27 @@
+ variables;
+
+
+A=b=c=20
+print (A)
+print(b)
+print(c)
+
+
+weight=23.52
+print(weight)
+
+
+x="python variables"
+print(x)
+X,M,Y= 4,7,"Element"
+print(X)
+print(M)
+print(Y)
+
+O = 30
+K = 10
+print(O+K)
+
+R="Computer Programming "
+I= "Today"
+print(R+I)
